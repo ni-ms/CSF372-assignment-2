8a01d03c1115b63da3e8f3bd49190d8d182bb39e
nikms <nikmsharma@gmail.com>
2022-11-24 13:30:54 +0530

    fixed some pointer stuff
    still broken :p

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <sys/time.h>
 
 //Concerns:
-//MMAP (fast for more i/o) or read file?
+//MMAP (fast for more i/o) or read file(fget)?
 //
 
 
@@ -69,50 +69,49 @@ int main(int argc, char * argv[]) {
     //NUMBER OF THREADS
     int n = 10;
 
-
+    //Thread id is stored
+    pthread_t *tid = (pthread_t*)malloc(n * sizeof(pthread_t));
     //Create i threads to read from file1
     for (int numOfThreads = 1; numOfThreads <= n; ++i) {
-        //Thread id is stored
-    pthread_t *tid = malloc(numOfThreads * sizeof(pthread_t));
 
-    //ALLOCATING THE MEMORY FOR 4D ARRAY
-    //Storing the first array
-    array1 *dat1 = malloc(i*sizeof(array1));
-    for(int temp = 0; temp < i; temp++){
-        dat1[temp].row = temp;
-        dat1[temp].list1 = malloc(j*sizeof(int));
-    }
+        //ALLOCATING THE MEMORY FOR 4D ARRAY
+        //Storing the first array
+        array1 *dat1 = (array1*)malloc(i*sizeof(array1));
+        for(int temp = 0; temp < i; temp++){
+            dat1[temp].row = temp;
+            //need to allocate to the list inside the datatype itself
+            dat1[temp].list1 = (int*)malloc(j*sizeof(int));
+        }
     /*
     To access the i,j
     dat1[i].list1[j];
     */
-     //Store the second array
-    array2 *dat2 = malloc(k*sizeof (array2));
+            //Store the second array
+        array2 *dat2 = (array2*)malloc(k*sizeof (array2));
         for (int temp = 0; temp < k; ++temp) {
             dat2[temp].col = temp;
-            dat2[temp].list2 = malloc(j*sizeof(int));
+            dat2[temp].list2 = (int*)malloc(j*sizeof(int));
 
         }
-    //CHECK VALUE
+ //CHECK VALUE
 
     //Store both array
-    RAS_t **datf_t = malloc(i*sizeof (RAS_t));
-        for (int p = 0; p < i; ++p) {
-            RAS_t *datf = malloc(k*sizeof (RAS_t));
-            datf_t[p] = datf;
-        }
+//    RAS_t **datf_t = malloc(i*sizeof (RAS_t));
+//        for (int p = 0; p < i; ++p) {
+//            RAS_t *datf = malloc(k*sizeof (RAS_t));
+//            datf_t[p] = datf;
+//        }
+//
+//        for (int num1 = 0; num1 < i; ++num1) {
+//            for (int num2 = 0; num2 < k; ++num2) {
+//                datf_t[num1][num2].rVal = dat1;
+//                datf_t[num1][num2].cVal = dat2;
+//            }
+//        }
 
-        for (int num1 = 0; num1 < i; ++num1) {
-            for (int num2 = 0; num2 < k; ++num2) {
-                datf_t[num1][num2].rVal = dat1;
-                datf_t[num1][num2].cVal = dat2;
-            }
-        }
-
-
-
-
-
+        RAS_t *finalAbs;
+        finalAbs->rVal = dat1;
+        finalAbs->cVal = dat2;
 
 
 
@@ -120,7 +119,7 @@ int main(int argc, char * argv[]) {
 
     for (int l = i; l > 0 ; l--) {
         //Thread arguments
-        if(pthread_create(&tid[i-l],NULL,readFileFun,(void*)datf) != 0){
+        if(pthread_create(&tid[i-l],NULL,readFileFun,(void*)finalAbs) != 0){
             fprintf( stderr, "ERROR: Could not create thread\n" );
             exit(EXIT_FAILURE);
         }
