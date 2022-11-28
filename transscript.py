26447fdead3783cbf25573c7e52ff0988013cb76
yashogunjal <51075317+yashogunjal@users.noreply.github.com>
2022-11-28 23:42:08 +0530

    Add files via upload

---
+++ transscript.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def fileopen():
+   # fr = open("in2.txt", 'r')
+   #fw = open("inp2.txt", 'w')
+   # array = [[int(number) for number in col.split()] for col in fr]
+   # fw.writelines(array)
+   array = np.loadtxt("in2.txt", dtype='i', delimiter = ' ')
+   array = array.transpose()
+   np.savetxt('inp2.txt', array, fmt= '%i')
+   # print(array, sep = '\n')
+fileopen()
