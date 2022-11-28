da229b9808091996c6d225095a479ec1e141ab54
Nikhil <nikmsharma@gmail.com>
2022-11-28 14:35:24 +0530

    updated files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/nik/Desktop/OS_GH/OS-assignment-2
+# Install script for directory: /home/nikms/Desktop/GitRepos/OS-assignment-2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/nik/Desktop/OS_GH/OS-assignment-2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/nikms/Desktop/GitRepos/OS-assignment-2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
