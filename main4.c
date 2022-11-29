7ae91711821a03b19e76262996a74bf7072777e7
yashogunjal <51075317+yashogunjal@users.noreply.github.com>
2022-11-29 19:40:21 +0530

    Add files via upload

--- main4.c
+++ main4.c
@@ -1,14 +1,13 @@
 //SCHEDULING
 #include <stdio.h>
 #include <stdlib.h>
-#include <
+//#include <
 
 #define P_NUM 2
 #define Quant1 1
 #define Quant2 2
 
-
-int NOP, sum = 0, count = 0;
+/*int sum = 0, count = 0;
 float avg_wt, avg_tat;
 int main(int argc, char * argv[]) {
 
@@ -16,5 +15,32 @@ int main(int argc, char * argv[]) {
         fork
     }
 
-}
+} */
 
+int main(){
+    pid_t p1;
+    pid_t p2;
+    p1  = fork();
+    
+    int status;
+
+    if (p1 == 0){
+        execlp("main2.out", "P1", "", NULL);
+    }
+
+    else if (p2 > 0){
+
+        p2 = fork();
+        
+        if (p2 == 0){
+
+        }
+
+        else if(p2 > 0){
+            nanosleep(1000000);
+            execlp("main3.out", "P2", "",NULL);
+        }
+
+
+    }
+}
